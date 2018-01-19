7bd1dcf22e09a264363cc1313eeb525625f6965a
Unknown <antoine35.colin@gmail.com>
2018-01-19 10:21:51 +0100

    Mais elles grattent

--- src/package2/Chaussettes.java
+++ src/package2/Chaussettes.java
@@ -3,6 +3,7 @@ package package2;
 public class Chaussettes {
 	boolean chaudes;
 	
+	
 	public Chaussettes(){
 		chaudes = true;
 	}
@@ -10,5 +11,6 @@ public class Chaussettes {
 	public static void main(String[] args){
 		System.out.println("les chaussettes sont elles chaudes ?");
 		System.out.println(new Chaussettes().chaudes);
+		System.out.println("mais elle grattent");
 	}
 }
