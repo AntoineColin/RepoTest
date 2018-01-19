e0c00538e0eb0abf85f635d19f7531051bd208b8
Unknown <antoine35.colin@gmail.com>
2018-01-19 10:16:58 +0100

    Chaussettes chaudes

---
+++ src/package2/Chaussettes.java
@@ -0,0 +1,14 @@
+package package2;
+
+public class Chaussettes {
+	boolean chaudes;
+	
+	public Chaussettes(){
+		chaudes = true;
+	}
+	
+	public static void main(String[] args){
+		System.out.println("les chaussettes sont elles chaudes ?");
+		System.out.println(new Chaussettes().chaudes);
+	}
+}
