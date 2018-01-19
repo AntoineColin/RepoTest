f64e4df2300c4f39086b479a07115ddf4b245a5e
Unknown <alexandrejaulin@orange.fr>
2018-01-19 10:19:07 +0100

    class coucou

---
+++ src/Coucou.java
@@ -0,0 +1,14 @@
+
+public class Coucou {
+	
+	String coucou = "coucou";
+	
+	static void direCoucou() {
+		System.out.println("coucou");
+	}
+	
+	public static void main(String[] args) {
+		direCoucou();
+	}
+
+}
